1bbec561b207df3a95d3cd2e68cd15aef7448624
kalumnoy <kalumnoy.38@gmail.com>
2016-10-12 12:48:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,14 +21,22 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello, world'
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [
+				{    	 "type":"text",
+           				 "text":"Hello, user"
+      				},
+       				{
+           				"type":"text",
+          				"text":"May I help you?"
+      				}
+					      ],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
